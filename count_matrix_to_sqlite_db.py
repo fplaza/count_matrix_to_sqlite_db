df6f585393364c6b377b728d2ca96ee3110f46c7
Florian Plaza Oñate <florian.plaza@jouy.inra.fr>
2016-03-15 14:23:24 +0100

    Create index on genes_names

--- count_matrix_to_sqlite_db.py
+++ count_matrix_to_sqlite_db.py
@@ -6,7 +6,7 @@
 from __future__ import print_function
 import argparse
 import sqlite3 
-from itertools import islice, chain
+from itertools import islice, izip, chain
 import sys
 
 __author__ = "Florian Plaza Oñate"
@@ -19,11 +19,15 @@ __status__ = "Development"
 def get_parameters():
     parser = argparse.ArgumentParser(description=__doc__)
 
-    parser.add_argument('--input-file', '-i', dest='count_matrix_txt', required=True, 
+    parser.add_argument('--count-matrix', '-i', dest='count_matrix_txt', required=True, 
             help='Count matrix with an header listing all the samples names. (tab-separated values file)')
 
-    parser.add_argument('--output-file', '-o', dest='count_matrix_db', required=True,
-            help='Generated SQLite database containing the count matrix data.')
+    parser.add_argument('--index-genes-names', dest='index_genes_names', action='store_true', default=False,
+            help='Indicates whether an index on genes names should be created.')
+
+    parser.add_argument('--output-db', '-o', dest='output_db', required=True,
+            help='Generated SQLite database.')
+
 
     return parser.parse_args()
 
@@ -41,7 +45,7 @@ def read_samples_names(count_matrix_txt):
         if not header.startswith('\t'):
             samples_names = header_items[1:]
 
-    print('Input file has {} samples'.format(len(samples_names)))
+    print('Count matrix file has {} samples\n'.format(len(samples_names)))
 
     return samples_names
 
@@ -50,42 +54,63 @@ def read_genes_profiles(istream):
 
     for line in istream:
         line_items = line.split()
-        gene_id = line_items[0]
+        gene_name = line_items[0]
         gene_profile = map(float, line_items[1:])
-        yield([gene_id] + gene_profile)
+        yield ([gene_name] + gene_profile)
 
-def create_count_matrix_table(count_matrix_db, samples_names):
-    req = 'CREATE TABLE GENES_PROFILES(GENE_ID INTEGER PRIMARY KEY AUTOINCREMENT, GENE_NAME TEXT NOT NULL,'
-    req += ','.join(( '"' + sample_name + '" REAL' for sample_name in samples_names)) + ');'
+def create_tables(samples_names, output_db):
+    genes_tbl_req = 'CREATE TABLE GENES(GENE_ID INTEGER PRIMARY KEY AUTOINCREMENT, GENE_NAME TEXT NOT NULL);'
 
-    conn = sqlite3.connect(count_matrix_db)
+    genes_profiles_tbl_req = 'CREATE TABLE GENES_PROFILES(GENE_ID INTEGER PRIMARY KEY AUTOINCREMENT,' 
+    genes_profiles_tbl_req += ','.join(( '"' + sample_name + '" REAL' for sample_name in samples_names)) + ');'
+
+    conn = sqlite3.connect(output_db)
     c = conn.cursor()
-    c.execute(req)
+    c.execute(genes_tbl_req)
+    c.execute(genes_profiles_tbl_req)
     conn.close()
 
-def fill_count_matrix_table(count_matrix_db, count_matrix_txt, num_samples, batch_size=1000):
-    req = 'INSERT INTO GENES_PROFILES VALUES (NULL,'
-    req += ','.join('?'*(num_samples+1))
-    req += ');'
+def fill_tables(count_matrix_txt, num_samples,output_db, batch_size=1000):
+    genes_tbl_req = 'INSERT INTO GENES VALUES(NULL,?)'
 
-    conn = sqlite3.connect(count_matrix_db)
+    genes_profiles_tbl_req = 'INSERT INTO GENES_PROFILES VALUES(NULL,'
+    genes_profiles_tbl_req += ','.join('?'*num_samples)
+    genes_profiles_tbl_req += ');'
+
+    conn = sqlite3.connect(output_db)
     c = conn.cursor()
 
     with open(count_matrix_txt) as istream:
         for i, genes_profiles in enumerate(batch(read_genes_profiles(istream),batch_size),start=1):
-            c.executemany(req, genes_profiles)
-            print("{} records inserted".format(i*batch_size))
+            genes_names=izip(item[0] for item in genes_profiles)
+            c.executemany(genes_tbl_req, genes_names)
+
+            genes_profiles=izip(item[1:] for item in genes_profiles)
+            c.executemany(genes_profiles_tbl_req, genes_profiles)
+
             sys.stdout.write("\033[F")
+            print("{} genes profiles processed".format(i*batch_size))
 
 
     conn.commit()
     conn.close()
 
+def create_genes_names_index(output_db):
+    req = "CREATE UNIQUE INDEX GENES_NAMES_INDEX ON GENES(gene_name);"
+
+    conn = sqlite3.connect(output_db)
+    c = conn.cursor()
+    c.execute(req)
+    conn.close()
+
 def main():
     parameters = get_parameters()
     samples_names = read_samples_names(parameters.count_matrix_txt)
-    create_count_matrix_table(parameters.count_matrix_db, samples_names)
-    fill_count_matrix_table(parameters.count_matrix_db,  parameters.count_matrix_txt, len(samples_names))
+    create_tables(samples_names, parameters.output_db)
+    fill_tables(parameters.count_matrix_txt, len(samples_names), parameters.output_db)
+    if (parameters.index_genes_names):
+        print("Creating a unique index on genes names")
+        create_genes_names_index(parameters.output_db)
 
 if __name__ == '__main__':
     main()
