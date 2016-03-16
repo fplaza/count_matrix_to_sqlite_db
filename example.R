d720e960cdfd0cac15dc8ad2da6f044bd11df724
Florian Plaza Oñate <florian.plaza@jouy.inra.fr>
2016-03-16 15:31:17 +0100

    Update example

--- example.R
+++ example.R
@@ -1,6 +1,6 @@
 library(RSQLite)
 con=dbConnect(RSQLite::SQLite(),dbname="IGC.raw_profiles.new.db")
 results=dbGetQuery(con,"SELECT * FROM genes NATURAL JOIN genes_profiles WHERE gene_id IN('5600000','690000','9898')")
-genes_ids=results[,"GENE_ID"]
-genes_names=results[,2]
-genes_profiles=results[,-c(1,2)]
+genes_ids=results[,"gene_id"]
+genes_names=results[,"gene_name"]
+genes_profiles=as.matrix(results[,-c(1,2)])
